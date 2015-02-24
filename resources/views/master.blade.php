71621a7e8d15527da28d59d40db6c08e071b7e78
Peter Justesen <pjust@justelund.dk>
2015-02-24 18:30:04 +0100

    ub
    A private project

---
+++ resources/views/master.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>United Buyers</title>
+  <link rel="stylesheet" type="text/css" href="{{ elixir('build/css/app.css') }}">
+</head>
+	<body>
+		<h1></h1>
+		<p></p>
+	</body>
+</html>
