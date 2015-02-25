3515136726e66c5d26da7da7c48267f8720a11d7
Peter Justesen <pjust@justelund.dk>
2015-02-25 12:47:20 +0100

    First Style Test
    Basic stuff

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,13 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+get('/', 'PagesController@index');
 
-Route::get('home', 'HomeController@index');
+get('about', 'PagesController@about');
+
+// Route::get('/', 'WelcomeController@index');
+
+// Route::get('home', 'HomeController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+
+@section('content')
+	about me
+@stop
+
+@section('footer')
+	<script type="text/javascript">
+
+	</script>
+@stop
