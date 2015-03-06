3818db6591124c4eab8dca8cb8af437492a017e1
Peter Justesen <pjust@justelund.dk>
2015-03-06 14:06:02 +0100

    6/3 -2

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,30 @@
 @extends('master')
 
 @section('content')
-	<div class="redmongol"></div>
+
+<div class="row" style=" box-shadow: 0px 0px 5px #888888; background:#FFF;">
+
+  <div class="col-md-6">
+  	<img src="{{ asset('/images/converse.jpg') }}" width="100%">
+  	<p style="width:100%; text-align:justify;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+
+  <div class="col-md-6">
+  	<p>Prisudvikling</p>
+	<img src="{{ asset('/images/graph.jpg') }}" width="100%">
+  </div>
+
+</div>
+
+<div class="row">
+  <div class="col-md-4">Start Pris:</div>
+  <div class="col-md-4">Pris Nu:</div>
+  <div class="col-md-4">Salg Slutter Om:</div>
+</div>
+
 @stop
