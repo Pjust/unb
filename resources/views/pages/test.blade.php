110d88cd5043dfd413cca17a91a12a7b2ed130ca
Peter Justesen <pjust@justelund.dk>
2015-03-09 11:52:44 +0100

    bla

--- resources/views/pages/test.blade.php
+++ resources/views/pages/test.blade.php
@@ -1,4 +1,5 @@
 
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,27 +7,39 @@
 <title>jQuery Countdown</title>
 <link rel="stylesheet" href="countdown/jquery.countdown.css">
 <style type="text/css">
+body > iframe { display: none; }
+
 #defaultCountdown { width: 240px; height: 45px; }
 </style>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script type="text/javascript" src="countdown/jquery.countdown.js"></script>
-<script type="text/javascript" src="countdown/jquery.plugin.js">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="countdown/jquery.plugin.js"></script>
+<script src="countdown/jquery.countdown.js"></script>
+<script>
 $(function () {
-  var austDay = new Date();
-  austDay = new Date(austDay.getFullYear() + 1, 1 - 1, 26);
-  $('#defaultCountdown').countdown({until: austDay});
-  $('#year').text(austDay.getFullYear());
+	var austDay = new Date();
+	austDay = new Date(austDay.getFullYear() + 1, 1 - 1, 26);
+	$('#defaultCountdown').countdown({until: austDay});
+	$('#year').text(austDay.getFullYear());
 });
 </script>
 </head>
 <body>
 <h1>jQuery Countdown Basics</h1>
 <p>This page demonstrates the very basics of the
-  <a href="http://keith-wood.name/countdown.html">jQuery Countdown plugin</a>.
-  It contains the minimum requirements for using the plugin and
-  can be used as the basis for your own experimentation.</p>
+	<a href="http://keith-wood.name/countdown.html">jQuery Countdown plugin</a>.
+	It contains the minimum requirements for using the plugin and
+	can be used as the basis for your own experimentation.</p>
 <p>For more detail see the <a href="http://keith-wood.name/countdownRef.html">documentation reference</a> page.</p>
-<p>Counting down to 26 January <span id="year">2010</span>.</p>
+<p>Counting down to 26 January <span id="year">2014</span>.</p>
 <div id="defaultCountdown"></div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-4715900-1");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
 </body>
 </html>
